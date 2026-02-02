7468b9b86149706449ab5f4d0adbf1080e85f848
Edwyn Jones <edwyn__4@hotmail.com>
2026-02-02 21:34:15 +0100

    updated image call

--- Components/adminComponents/BlogTableItem.jsx
+++ Components/adminComponents/BlogTableItem.jsx
@@ -2,13 +2,13 @@ import { assets } from '@/Assets/assets'
 import Image from 'next/image'
 import React from 'react'
 
-const BlogTableItem = ({authorImg, title, author, date, deleteBlogs, mongoId}) => {
+const BlogTableItem = ({authorImage, title, author, date, deleteBlogs, mongoId}) => {
     const BlogDate = new Date(date)
 
   return (
     <tr className='bg-white border-b'>
         <th scope='row' className='items-center gap-3 hidden sm:flex px-6 py-4 font-medium text-gray-900 whitespace-nowrap'>
-            <Image width={40} height={40} src={authorImg ? authorImg : assets.profile_icon} alt=''/>
+            <Image width={40} height={40} src={authorImage ? authorImage : assets.profile_icon} alt=''/>
             <p> {author ? author : 'No Author'} </p>
         </th>
         <td className='px-6 py-4'>
@@ -24,4 +24,4 @@ const BlogTableItem = ({authorImg, title, author, date, deleteBlogs, mongoId}) =
   )
 }
 
-export default BlogTableItem
+export default BlogTableItem;
